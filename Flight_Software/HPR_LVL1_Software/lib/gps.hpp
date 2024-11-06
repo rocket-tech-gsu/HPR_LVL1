97f125ea019c15fff05665a44596ba3f977e5324
Chisom Maxwell <chisomm27@gmail.com>
2024-11-06 15:47:24 -0500

    GPS module working!!

--- Flight_Software/HPR_LVL1_Software/lib/gps.hpp
+++ Flight_Software/HPR_LVL1_Software/lib/gps.hpp
@@ -1,95 +1,123 @@
-#include <vector>
-#include <sstream>
-
-struct Location {
-    std::string latitude;
-    std::string longitude;
-    std::string altitude;
-    std::string time;
-    bool valid;
-};
+#include <Arduino.h>
+#include <HardwareSerial.h>
 
 class GPS{
     private:
+        int RX;
+        int TX;
+        HardwareSerial &gpsSerial;
     public:
-        std::vector<std::string> GPS::parsed_data(std::string& str);
-        Location GPS::get_loco(std::string& str);
-        bool GPS::startup(Location loco);
+        GPS(HardwareSerial &serial) : gpsSerial(serial) {}
+
+        HardwareSerial &getGPSSerial();
+        void set_rx_tx(int r, int t);
+        int get_rx();
+        int get_tx();
+        String* parsed_data(String nmea);
+        String get_latitude(String* fields);
+        String get_longitude(String* fields);
+        float get_altitude(String* fields);
+        bool is_fixed(String* fields); 
+        bool startup(int r, int t);     
+        
 };
 
 //Parses the data into an array of strings with fields
-std::vector<std::string> GPS::parsed_data(std::string& str)
-{
-    std::vector<std::string> result;
-    size_t pos = 0;
-    std::string part;
 
-    while ((pos = str.find(',')) != std::string::npos)
-    {
-        part = str.substr(0,pos);
-        result.push_back(part);
-        str.erase(0, pos + 1);
-    }
-    result.push_back(str);
-
-    return result;
+HardwareSerial& GPS::getGPSSerial() 
+{ 
+    return gpsSerial; 
 }
 
-Location GPS::get_loco(std::string& str)
+void GPS::set_rx_tx(int r, int t)
 {
-    struct Location loco;
-    std::vector<std::string> fields = parsed_data(str);
-    /*
-        TODO get latitude, longitude, and altitude
-            make a case statement for:
-                $GPGGA sentance--Done
-                $GPGGA,170834,4124.8963,N,08151.6838,W,1,05,1.5,280.2,M,-34.0,M,,,*59
-                $GPRMC sentance--InProg
-                $GPGSV sentance--InProg
-                $GPGLL sentance--InProg
-                $GPVTG sentance--InProg
-                $GPGSA sentance--InProg
-    */ 
-    if (fields[0] == "$GPGGA")
-    {   
-        //getting values of latitude and longitude as well as if the gps signal is valid(All are strings)
-        loco.latitude = fields[2].substr(0, fields[2].length() - (fields[2].length() - fields[2].find("."))) + "d " + fields[2].substr(fields[2].find(".") + 1,7) + "' " + fields[3];
-        loco.longitude = fields[4].substr(0, fields[4].length() - (fields[4].length() - fields[4].find("."))) + "d " + fields[4].substr(fields[4].find(".") +1,7) + "' " + fields[5];
-        loco.time = fields[1].substr(0,2) + ":" + fields[1].substr(2,2) + ":" + fields[1].substr(4,2) + " UTC";
-        loco.altitude = fields[9];
-        if ((fields[6] == "1") || (fields[6] == "2"))
-        {
-            loco.valid = true;
-        } else {
-            loco.valid = false;
+    RX = r;
+    TX = t;
+}
+int GPS::get_rx()
+{
+    return RX;
+}
+int GPS::get_tx()
+{
+    return TX;
+}
+bool GPS::startup(int r, int t)
+{
+    try{
+        RX = r;
+        TX = t;
+
+        gpsSerial = HardwareSerial(1);
+        gpsSerial.begin(9600, SERIAL_8N1, RX, TX);
+
+        // Add the code you provided
+        gpsSerial.println("$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"); // Set NMEA output
+        gpsSerial.println("$PMTK220,1000*1F"); // Set 1 Hz update rate
+        return true;
+    } catch(bool) {
+        Serial.printf("");
+        return false;
+    }
+    
+}
+
+String* GPS::parsed_data(String nmea) {
+    String* fields = new String[15];  // Create a dynamic array for fields
+    if (nmea.startsWith("$GPGGA")) {
+        int nxt_index = 0;
+        int substr_start = 0;
+
+        for (int i = 0; i < nmea.length(); i++) {
+            if (nmea.charAt(i) == ',' || i == nmea.length() - 1) {
+                if (i == nmea.length() - 1) {
+                    i++; // Include last character if we reach the end without a comma
+                }
+
+                if (nxt_index < 15) { // Avoid overflow
+                    fields[nxt_index] = nmea.substring(substr_start, i);
+                    nxt_index++;
+                }
+
+                substr_start = i + 1; // Move start to character after comma
+            }
         }
-    //Not sure if I should code all of these but I will comment them out just in case
-    // } else if (fields[0] == "$GPRMC")
-    // {
-
-    // } else if (fields[0] == "$GPGSV")
-    // {
-
-    // } else if (fields[0] == "$GPGLL")
-    // {
-
-    // } else if (fields[0] == "$GPVTG")
-    // {
-
-    // } else if (fields[0] == "$GPGSA")
-    // {
-
-    } else {
-        //Catch all (error message)
-        fprintf(stderr, "Invalid sentance type.\n");
     }
-
-    return loco;
+    return fields;
 }
 
-bool GPS::startup(Location loco)
+
+String GPS::get_latitude(String* fields)
 {
-    if (loco.valid == true)
+    if (fields[0].equals("$GPGGA") && !(fields[2].equals("")))
+    {
+        return fields[2] + ", " + fields[3];
+    } else {
+        return "No info on Latitude";
+    }
+}
+String GPS::get_longitude(String* fields)
+{
+    if (fields[0].equals("$GPGGA") && !(fields[4].equals("")))
+    {
+        return fields[4] + ", " + fields[5];
+    } else {
+        return "No info on Longitude";
+    }
+    
+}
+float GPS::get_altitude(String* fields)
+{
+    if (!fields[9].equals(""))
+    {
+        return fields[9].toFloat();
+    } else {
+        return -1.00;
+    }
+}
+bool GPS::is_fixed(String* fields)
+{
+    if ((fields[6] == "1") || (fields[6] == "2"))
     {
         return true;
     } else {
@@ -98,26 +126,17 @@ bool GPS::startup(Location loco)
 }
 
 /*
-    How to Use:
-        1. In the main loop, first check if the gps signal is available. ex:
-            if (gpsSerial.available()) {
-                //Do Stuff
-            } else {
-                Serial.println("Error Message");
-            }
-        2. If the gpsSerial is available, we have access to the location variables, latitude, longitude, altitude, and time
-            String nmea = gpsSerial.readStringUntil('\n'); //contains the information we need
-            Location loc = GPS::get_loco(nmea);            //parses the line and stores it into a location variable
-            
-            if (loc.valid) {                               //If the GPS is fixed(valid), we can print out and see it's contents 
-                Serial.print("Latitude: ");                //So far they aren't numerical yet, I will change this soon but this is just to view its contents
-                Serial.println(loc.latitude);
-                Serial.print("Longitude: ");
-                Serial.println(loc.longitude);
-                Serial.print("Altitude: ");
-                Serial.println(loc.altitude);
+    How to use:
 
-            } else {                                       //If signal is not fixed, it is invalid, so we can't use it
-                Serial.println("Invalid or No Fix");
-            }
+    Outside setup and loop functions:
+    //make sure 
+    GPS gps(Serial1);  // Initialize GPS with Serial1
+    HardwareSerial gpsSerial(1);
+
+    Inside setup function
+
+    void setup() {
+        Serial.begin(115200);
+        gps.startup(GPS_RX_PIN, GPS_TX_PIN);
+    }
 */
