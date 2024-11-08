561c35aee0acb7e68e6a1a1f343f04faf246c278
Varun Ahlawat <110617721+yup-VARUN@users.noreply.github.com>
2024-11-08 18:28:22 -0500

    Header FIles for GY_61 accelerometer and SD_Card

--- Flight_Software/HPR_LVL1_Software/src/main.cpp
+++ Flight_Software/HPR_LVL1_Software/src/main.cpp
@@ -1,13 +1,29 @@
+// Header Files:
 #include <Arduino.h>
+#include <../lib/GPS_Module.h>
+#include <../lib/BMP_Module.h>
+#include <../lib/MPU6050_Module.h>
+#include <../lib/SD_Card_Module.h>
 
-// put function declarations here:
+
+//Objects Declarations(from the classes present in the header files):
+
+
+// Function Declarations:
 int myFunction(int, int);
+
+// Variable Declarations:
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
   pinMode(LED_BUILTIN, OUTPUT);
   int result = myFunction(2, 3);
+
 }
 
 void loop() {
